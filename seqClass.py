18e5dab78cdf5ee11ff9a6c92c7cac7330ccf678
Nerea Ye Keituqwa Rebollo <nereaye@MacBook-Air-de-Nerea.local>
2026-01-26 18:50:57 +0100

    ah

--- seqClass.py
+++ seqClass.py
@@ -27,7 +27,7 @@ if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-        print("YES")
+        print("AAA")
     else:
         print("NO")
 
