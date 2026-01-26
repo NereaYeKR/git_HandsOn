2e009299f75aef88a39a5f0e1b95977f62ecc18f
Nerea Ye Keituqwa Rebollo <nereaye@MacBook-Air-de-Nerea.local>
2026-01-26 18:15:19 +0100

    motif and parser

--- seqClass.py
+++ seqClass.py
@@ -4,7 +4,7 @@ import sys, re
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
-parser.add_argument("-s", "--seq", type = str, required = True, help = "aaa")
+parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -22,3 +22,13 @@ if re.search('^[ACGTU]+$', args.seq):
         print ('The sequence can be DNA or RNA')
 else:
     print ('Error not DNA or RNA')
+
+if args.motif:
+    args.motif = args.motif.upper()
+    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
+    if re.search(args.motif, args.seq):
+        print("FOUND")
+    else:
+        print("NOT FOUND")
+
+parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
