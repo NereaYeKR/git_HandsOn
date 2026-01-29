eda42495a2e68cc31e516ee257f29e8298926557
Nerea Ye Keituqwa Rebollo <nereaye@MacBook-Air-de-Nerea.local>
2026-01-29 13:34:46 +0100

    Fix errors

--- seqClass.py
+++ seqClass.py
@@ -18,19 +18,17 @@ if re.search('^[ACGT]+$', args.seq):  # Check if it's DNA
     print('The sequence is DNA')
 elif re.search('^[ACGU]+$', args.seq):  # Check if it's RNA
     print('The sequence is RNA')
-else:  # Invalid sequence
-    print('Error: the sequence is not valid DNA or RNA sequence')
-    print('DNA sequences can only contain A, C, G, T')
-    print('RNA sequences can only contain A, C, G, U')
+else: 
+    print('The sequence is not DNA or RNA')
 
 # Search for a motif
 if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end='')
     if re.search(args.motif, args.seq):
-        print("Motif found in sequence")
+        print("FOUND")
     else:
-        print("Motif not found in sequence")
+        print("NOT FOUND")
 
 
 
