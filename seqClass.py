2c42b80f610f3a322ae528cb2e9ad9cefac9dcef
Nerea Ye Keituqwa Rebollo <nereaye@MacBook-Air-de-Nerea.local>
2026-01-29 14:30:03 +0100

    fix

--- seqClass.py
+++ seqClass.py
@@ -13,17 +13,16 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-args.seq = args.seq.upper()
+args.seq = args.seq.upper()#Converts the sequence to uppercase to ensure case insensitivity
 
-if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
-        print ('The sequence is DNA')
-    elif re.search('U', args.seq):
-        print ('The sequence is RNA')
-    else:
-        print ('The sequence can be DNA or RNA')
-else:
-    print ('The sequence is not DNA nor RNA')
+if re.search('^[ACGT]+$', args.seq):  # Check if it's DNA
+    print('The sequence is DNA')
+elif re.search('^[ACGU]+$', args.seq):  # Check if it's RNA
+    print('The sequence is RNA')
+else: 
+    print('The sequence is not DNA or RNA')
+
+#Search for a motif
 
 if args.motif:
     args.motif = args.motif.upper()
